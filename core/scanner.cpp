f027516888ea626069c00bb2aa37fb70c13d8579
Tuukka Norri <tsnorri@iki.fi>
2025-06-13 15:27:50 +0300

    Apply formatting by Zed

--- core/scanner.cpp
+++ core/scanner.cpp
@@ -30,16 +30,16 @@ namespace MOODS { namespace scan{
         l = window_size;
 
         alphabet_map = vector<unsigned char>(256, 4);
-        
+
         alphabet_map[(unsigned char)'a'] = 0;
         alphabet_map[(unsigned char)'A'] = 0;
-        
+
         alphabet_map[(unsigned char)'c'] = 1;
         alphabet_map[(unsigned char)'C'] = 1;
-        
+
         alphabet_map[(unsigned char)'g'] = 2;
         alphabet_map[(unsigned char)'G'] = 2;
-        
+
         alphabet_map[(unsigned char)'t'] = 3;
         alphabet_map[(unsigned char)'T'] = 3;
 
@@ -63,13 +63,13 @@ namespace MOODS { namespace scan{
         initialised = false;
         max_motif_size = 0;
     }
-    
+
     void Scanner::set_motifs(const std::vector<score_matrix>& matrices,
                         const std::vector<double>& bg,
                         const std::vector<double> thresholds){
 
         this->motifs = vector<unique_ptr<Motif>>();
-        
+
         for (size_t i = 0; i < matrices.size(); ++i){
             if (matrices[i].size() == a){
                motifs.emplace_back(new Motif0(matrices[i], bg, l, thresholds[i]));
@@ -79,7 +79,7 @@ namespace MOODS { namespace scan{
             }
             this->max_motif_size = std::max(this->max_motif_size, motifs.back()->size());
         }
-        
+
         this->initialise_hit_table();
 
     }
@@ -88,18 +88,18 @@ namespace MOODS { namespace scan{
 
         const bits_t SHIFT = MOODS::misc::shift(a);
         const bits_t CODE_SIZE = 1 << (SHIFT * l);
-        
+
         window_hits = vector<vector<scanner_output> >(CODE_SIZE, vector<scanner_output>());
-        
+
         for (bits_t code = 0; code < CODE_SIZE; ++code)
         {
             for (size_t k = 0; k < motifs.size(); ++k)
             {
                 double score;
                 bool match;
-                
+
                 std::tie(match,score) = motifs[k]->window_match(code, SHIFT);
-                
+
                 if (match)
                 {
                     scanner_output op = {score, k, motifs[k]->size() <= l};
@@ -111,10 +111,10 @@ namespace MOODS { namespace scan{
         initialised = true;
     }
 
-    
+
     // the template for various scan functions
     // the match_handler handles the actual logic of processing matches
-    template<typename T> 
+    template<typename T>
     void Scanner::process_matches(const std::string& s, T& match_handler)
     {
 
@@ -124,24 +124,24 @@ namespace MOODS { namespace scan{
 
         const bits_t SHIFT = MOODS::misc::shift(a);
         const bits_t MASK = (1 << (SHIFT * l)) - 1;
-        
+
         vector<size_t> bounds = misc::preprocess_seq(s, this->a, this->alphabet_map);
-        
+
         // Scanning
         for (size_t seq_i = 0; seq_i < bounds.size(); ){
             size_t start = bounds[seq_i];
             ++seq_i;
             size_t end = bounds[seq_i];
             ++seq_i;
-            
-            
+
+
             // sequence is very short
             if (end - start < l){
-                
+
                 bits_t code = 0;
                 for (size_t i = start; i < end; ++i)
                     code = (code << SHIFT) + alphabet_map[s[i]];
-                
+
                 for (size_t i = end - start; i < l - 1; ++ i){
                     code = (code << SHIFT) & MASK;  // dummy character to the end of code
                 }
@@ -150,7 +150,7 @@ namespace MOODS { namespace scan{
                     if (match_handler.has_hits(code))
                     {
                         code = (code << SHIFT) & MASK;  // dummy character to the end of code
-                        
+
                         for (const scanner_output& y : match_handler.hits(code))
                         {
                             if (y.full && motifs[y.matrix]->size() <= end - i) // only sufficiently short hits are considered
@@ -169,7 +169,7 @@ namespace MOODS { namespace scan{
                 for (size_t i = start; i < start + l - 1; ++i){
                     code = (code << SHIFT) + alphabet_map[s[i]];
                 }
-                
+
                 // Actual scanning for the 'middle' of the sequence
                 for (size_t i = start; i < end - l + 1; ++i)
                 {
@@ -198,15 +198,15 @@ namespace MOODS { namespace scan{
                         match_handler.clean_up();
                     }
                 }
-            
+
                 // possible hits for matrices shorter than l near the end of current interval
                 for (size_t i = end - l + 1; i < end; ++i)
                 {
                     code = (code << SHIFT) & MASK;  // dummy character to the end of code
-            
+
                     if (match_handler.has_hits(code))
                     {
-                        
+
                         for (const scanner_output& y : match_handler.hits(code))
                         {
                             if (y.full && motifs[y.matrix]->size() < end - i) // only sufficiently short hits are considered
@@ -218,13 +218,13 @@ namespace MOODS { namespace scan{
                         match_handler.clean_up();
                     }
                 }
-            } 
+            }
         }
     }
 
 
     // basic match handler
-    // stores matches and returns all of them 
+    // stores matches and returns all of them
     class AllHitsMH{
     public:
         AllHitsMH(size_t motifs, std::vector<std::vector<scanner_output>>& _hits) : window_hits(_hits)
@@ -394,8 +394,8 @@ namespace MOODS { namespace scan{
 
     void Scanner::variant_matches_recursive(std::vector<std::vector<match_with_variant>>& results, const state& current,
                                             const std::string& seq, const std::vector<variant>& variants, int max_depth){
-        
-        size_t first_required_index = current.variant_start_pos + variants[current.vs.front()].modified_seq.size() - 1;         
+
+        size_t first_required_index = current.variant_start_pos + variants[current.vs.front()].modified_seq.size() - 1;
         size_t last_required_index = current.prefix.size() - variants[current.vs.back()].modified_seq.size();
 
         size_t remaining = 0;
@@ -407,7 +407,7 @@ namespace MOODS { namespace scan{
         string current_seq = current.prefix + seq.substr(variants[current.vs.back()].end_pos, remaining);
 
         vector<vector<match>> current_results = this->scan(current_seq);
-        
+
         for (size_t motif = 0; motif < current_results.size(); ++motif){
             for (size_t i = 0; i < current_results[motif].size(); ++i){
                 match m = current_results[motif][i];
@@ -415,11 +415,11 @@ namespace MOODS { namespace scan{
                 if (m.pos + this->motifs[motif]->size() >= last_required_index + 1 && m.pos <= first_required_index) {
                     results[motif].push_back(match_with_variant{m.pos + current.seq_start_pos,m.score,current.vs});
                 }
-            }    
+            }
         }
 
         // second, recursive call for each possible variant that can follow the current last variant
-        
+
         if (current.depth != max_depth){
             for (size_t next_variant = current.vs.back() + 1; next_variant < variants.size(); ++next_variant){
 
@@ -443,7 +443,7 @@ namespace MOODS { namespace scan{
                     string next_prefix = current.prefix + seq.substr(cv.end_pos, nv.start_pos - cv.end_pos) + nv.modified_seq;
                     auto next_variants = current.vs;
                     next_variants.push_back(next_variant);
-                    
+
                     state next = {next_variants, next_prefix, current.seq_start_pos, current.variant_start_pos, current.depth + 1};
                     this->variant_matches_recursive(results, next, seq, variants, max_depth);
                 }
@@ -453,31 +453,31 @@ namespace MOODS { namespace scan{
 
 
     std::vector<std::vector<match_with_variant>> Scanner::variant_matches(const std::string& seq, const std::vector<variant>& _variants, int max_depth){
-        
-        
+
+
         // copy and sort the variants
         // we'll need to keep track of the original order of the variants for output purposes
         std::vector< std::pair<variant, size_t> > v;
-        
+
         for (size_t i = 0; i < _variants.size(); ++i){
             std::pair<variant,size_t> P = std::make_pair(_variants[i],i);
             v.push_back(P);
         }
         std::sort(v.begin(), v.end());
-        
+
         std::vector<variant> variants;
         std::vector<size_t> variants_permutation;
-        
+
         for (size_t i = 0; i < v.size(); ++i){
             variants.push_back(v[i].first);
             variants_permutation.push_back(v[i].second);
         }
-        
+
         vector<vector<match_with_variant>> results(this->size(), vector<match_with_variant>());
 
         for (size_t i = 0; i < variants.size(); ++i){
-            // we'll skip all deletions that start at position 0 or 
-            // end at the last position 
+            // we'll skip all deletions that start at position 0 or
+            // end at the last position
             // as that does not really make any sense in our framework
             if (! ( (variants[i].start_pos == 0 && variants[i].modified_seq.size() == 0) ||
                     (variants[i].end_pos == seq.size() && variants[i].modified_seq.size() == 0))) {
@@ -491,20 +491,20 @@ namespace MOODS { namespace scan{
                             prefix_start = 0;
                             prefix_length = variants[i].start_pos;
                         }
-        
+
                         string next_prefix = seq.substr(prefix_start, prefix_length) + variants[i].modified_seq;
-        
+
                         state s = {vector<size_t>(1,i), // active variants
-                                   next_prefix, // prefix string 
+                                   next_prefix, // prefix string
                                    prefix_start, // seq position where prefix starts
-                                   prefix_length, // first position in prefix that is from the modified string 
+                                   prefix_length, // first position in prefix that is from the modified string
                                    1 // recursion depth
                                   };
-        
+
                         this->variant_matches_recursive(results, s, seq, variants, max_depth);
             }
         }
-        
+
         // fix indexing for variants stored in matches
         // TODO: do this immediately when constructing matches instead of postprocessing?
         for (size_t m = 0; m < results.size(); ++m){
@@ -514,7 +514,7 @@ namespace MOODS { namespace scan{
                 }
             }
         }
-        
+
         return results;
     }
 
